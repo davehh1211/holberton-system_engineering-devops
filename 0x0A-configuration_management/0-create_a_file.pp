774d871107b90608f794ac01ab6937145a74d5b9
davehh1211 <davehh1211@gmail.com>
2020-08-03 20:50:22 +0000

    task 0.1 fix manifest

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,7 +3,7 @@
 file { 'holberton':
   path    => '/tmp/holberton',
   content => 'I love Puppet',
-	mode    => '0744',
-	owner   => 'www-data',
-	group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
