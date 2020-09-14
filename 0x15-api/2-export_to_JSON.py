c0bf3cca108792d3c5291ca7737318657ec6c7cc
davehh1211 <davehh1211@gmail.com>
2020-09-14 23:06:04 +0000

    task 2.3

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -15,15 +15,15 @@ if __name__ == "__main__":
         'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
             USER_ID)).json()
 
-dict_task = []
-for task in list_to_do:
-    task_field = {}
-    task_field['task'] = task.get('title')
-    task_field['completed'] = task.get('completed')
-    task_field['username'] = user.get('username')
-    dict_task.append(task_field)
+    dict_task = []
+    for task in list_to_do:
+        task_field = {}
+        task_field['task'] = task.get('title')
+        task_field['completed'] = task.get('completed')
+        task_field['username'] = user.get('username')
+        dict_task.append(task_field)
 
-task_json = {}
-task_json[USER_ID] = dict_task
-with open('{}.json'.format(USER_ID), 'w') as file:
-    json.dump(task_json, file)
+    task_json = {}
+    task_json[USER_ID] = dict_task
+    with open('{}.json'.format(USER_ID), 'w') as file:
+        json.dump(task_json, file)
