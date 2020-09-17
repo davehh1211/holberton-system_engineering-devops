a3c45b6f3f0f8bc3e65ce729833bb6ab2cd92fc7
davehh1211 <davehh1211@gmail.com>
2020-09-17 22:03:23 +0000

    task 2.1

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -11,11 +11,12 @@ def recurse(subreddit, hot_list=[], after=''):
                              format(subreddit, after),
                              headers={'user-agent': 'fake_user_agent'},
                              allow_redirects=False).json()
-    after = query_sub.get('data').get('after', None)
+    after = query_sub.get('data', {}).get('after', None)
     hot_children = query_sub.get('data', {}).get('children', [])
     if hot_children is None:
         if len(hot_children) == 0:
             return None
+        return hot_list
     else:
         for titles in hot_children:
             hot_list.append(titles.get('data').get('title'))
