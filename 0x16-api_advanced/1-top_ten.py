0010345411571c07d089f4755020a99525636dc8
davehh1211 <davehh1211@gmail.com>
2020-09-17 20:13:43 +0000

    task 1.0

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""[summary"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Args:
+        subreddit (query): [description]
+    """
+    if subreddit is None or type(subreddit) is not str:
+        print(None)
+    query_sub = requests.get('https://www.reddit.com/r/{}/hot.json'.format(
+        subreddit), headers={'user-agent': 'fake_user_agent'}).json()
+    sub_number = query_sub.get('data', {}).get('children', None)
+    if not sub_number:
+        print(None)
+    for tops in sub_number[:10]:
+        print(tops.get('data', {}).get('title', None))
