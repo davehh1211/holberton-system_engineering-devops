ad13ff76f71ea720a8474bf49fe823e14e44b090
davehh1211 <davehh1211@gmail.com>
2020-09-17 20:33:49 +0000

    task 1.2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,11 +10,10 @@ def top_ten(subreddit):
     """
     if subreddit is None or type(subreddit) is not str:
         print(None)
-    query_sub = requests.get('https://www.reddit.com/r/{}/hot.json'.format(
-        subreddit), headers={'user-agent': 'fake_user_agent'}, allow_redirects=False).json()
-    sub_number = query_sub.get('data', {}).get('children', None)
+    query_sub = requests.get('https://www.reddit.com/r/{}/hot.json'.
+                             format(subreddit), headers={'user-agent': 'fake_user_agent'}, allow_redirects=False).json()
+    sub_number = query_sub.get('data', {}).get('children', [])
     if not sub_number:
         print(None)
-    else:
-        for tops in sub_number[:10]:
-            print(tops.get('data', {}).get('title', None))
+    for tops in sub_number[:10]:
+        print(tops.get('data').get('title'))
